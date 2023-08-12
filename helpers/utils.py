d6f9df5e84722ce1f2b154c12c76647ed79299be
Matheus Victor <matheusvictor.salles@gmail.com>
2023-08-12 16:28:51 -0300

    refactor: find_court_from_cnj method
    additional: using Black tool to indentation

--- helpers/utils.py
+++ helpers/utils.py
@@ -2,30 +2,30 @@ import json
 import os
 import re
 
-CNJ_PATTERN = r'^\d{7}-\d{2}.\d{4}.\d{1}.\d{2}.\d{4}$'
+CNJ_PATTERN = r"^\d{7}-\d{2}.\d{4}.\d{1}.\d{2}.\d{4}$"
 
 
 def save_json_file(data, file_name: str):
     json_object = json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
 
-    if not file_name.endswith('.json'):
-        file_name += '.json'
+    if not file_name.endswith(".json"):
+        file_name += ".json"
 
-    path = os.path.join('output', file_name)
-    os.makedirs('output', exist_ok=True)
+    path = os.path.join("output", file_name)
+    os.makedirs("output", exist_ok=True)
 
-    with open(path, "w", encoding='utf8') as outfile:
+    with open(path, "w", encoding="utf8") as outfile:
         outfile.write(json_object)
         outfile.close()
 
 
 def remove_duplicated_space_and_line_breaks_from_cnj_input(cnj: str):
-    return re.sub(pattern=r'\s+', repl=' ', string=cnj.strip())
+    return re.sub(pattern=r"\s+", repl=" ", string=cnj.strip())
 
 
 def remove_special_symbols_from_string(string: str):
-    regex_pattern = r'[\n\t]'
-    return re.sub(regex_pattern, '', string)
+    regex_pattern = r"[\n\t]"
+    return re.sub(regex_pattern, "", string)
 
 
 def is_cnj_format_valid(cnj: str) -> bool:
@@ -35,8 +35,12 @@ def is_cnj_format_valid(cnj: str) -> bool:
 
 
 def remove_special_symbols_from_cnj(cnj: str):
-    return cnj.replace('-', '').replace('.', '')
+    return cnj.replace("-", "").replace(".", "")
 
 
-def find_court_from_cnj_by_regex(cnj: str, court_code: str):
-    return re.search(court_code, cnj)
+def find_court_from_cnj(cnj: str) -> str or None:
+    regex = re.compile(r"\.\d\.\d{2}\.")
+    result = regex.search(cnj)
+    if result:
+        return result.group()[1:-1]
+    return None
