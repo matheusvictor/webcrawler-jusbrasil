17b86fc2ed3cfdd18075b4a5c1563d7145946991
Matheus Victor <matheusvictor.salles@gmail.com>
2023-08-12 17:03:36 -0300

    fix: update constants values

--- constants/constants.py
+++ constants/constants.py
@@ -1,8 +1,8 @@
-TJAL_CODE = '8.02'
-TJCE_CODE = '8.06'
+TJAL_CODE = "8.02"
+TJCE_CODE = "8.06"
 
-FIRST_LEGAL_INSTANCE_TJAL: str = 'https://www2.tjal.jus.br/cpopg/open.do'
-SECOND_LEGAL_INSTANCE_TJAL: str = 'https://www2.tjal.jus.br/cposg5/open.do'
+FIRST_LEGAL_INSTANCE_TJAL: str = "https://www2.tjal.jus.br/cpopg/show.do"
+SECOND_LEGAL_INSTANCE_TJAL: str = "https://www2.tjal.jus.br/cposg5/open.do"
 
-FIRST_LEGAL_INSTANCE_TJCE: str = 'https://esaj.tjce.jus.br/cpopg/open.do'
-SECOND_LEGAL_INSTANCE_TJCE = 'https://esaj.tjce.jus.br/cposg5/open.do'
+FIRST_LEGAL_INSTANCE_TJCE: str = "https://esaj.tjce.jus.br/cpopg/show.do"
+SECOND_LEGAL_INSTANCE_TJCE = "https://esaj.tjce.jus.br/cposg5/open.do"
