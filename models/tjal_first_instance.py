959fc2ece664354293e93e160fdd97fd740d1b5d
Matheus Victor <matheusvictor.salles@gmail.com>
2023-08-15 21:30:42 -0300

    refactor: change url value
    additional: remove mock body from TjceFirstInstance constructor

--- models/tjal_first_instance.py
+++ models/tjal_first_instance.py
@@ -1,12 +1,12 @@
 from constants import constants
-from models.crawler_esaj import CrawlerEsaj
+from models.crawler_esaj_first_instance import CrawlerFirstInstanceEsaj
 
 
-class TjalFirstInstance(CrawlerEsaj):
+class TjalFirstInstance(CrawlerFirstInstanceEsaj):
     @property
     def court_code(self) -> str:
         return constants.TJAL_CODE
 
     @property
     def url(self) -> str:
-        return f"https://www2.tjal.jus.br/cpopg/show.do?processo.numero={self.cnj}"
+        return f"https://www2.tjal.jus.br/cpopg/show.do"
--- models/tjce_first_instance.py
+++ models/tjce_first_instance.py
@@ -1,17 +1,12 @@
 from constants import constants
-from mocks.mocks import create_bs4_object_from_tjce
-from models.crawler_esaj import CrawlerEsaj
+from models.crawler_esaj_first_instance import CrawlerFirstInstanceEsaj
 
 
-class TjceFirstInstance(CrawlerEsaj):
-    def __init__(self, base_url: str, cnj: str):
-        super().__init__(base_url, cnj)
-        self._body = create_bs4_object_from_tjce().body
-
+class TjceFirstInstance(CrawlerFirstInstanceEsaj):
     @property
     def court_code(self) -> str:
         return constants.TJCE_CODE
 
     @property
     def url(self) -> str:
-        return f"https://esaj.tjce.jus.br/cpopg/open.do?processo.numero={self.cnj}"
+        return f"https://esaj.tjce.jus.br/cpopg/show.do"
